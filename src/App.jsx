6705f98c40455e5afa83a02dd348fd9a2c7b5e1b
MOHAMMAD ASIF <asifmohd1146@gmail.com>
2023-11-28 15:42:58 +0530

    route updates

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,9 @@ function App() {
           <Suspense fallback={""}>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="projects" element={<Projects />} />
-              <Route path="about" element={<About />} />
-              <Route path="contact" element={<Contact />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
               <Route path="*" element={<Error />} />
             </Routes>
           </Suspense>
