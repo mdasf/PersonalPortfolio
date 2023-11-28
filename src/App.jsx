f3bb33ec52a08c58cc9934006cfe44021b011e20
MOHAMMAD ASIF <asifmohd1146@gmail.com>
2023-11-28 16:08:41 +0530

    added redirects file

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           <AppHeader />
           <Suspense fallback={""}>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" index element={<Home />} />
               <Route path="/projects" element={<Projects />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
